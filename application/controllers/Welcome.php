d0ed5191e6759e987797f45204b2e94ec250fd60
fccpradeep <pradeep.Tcrystaltech@gmail.com>
2020-06-09 15:14:28 +0530

    add payment gaytwaycode

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,129 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('product_form');		
+	}
+
+	public function check()
+	{
+		//check whether stripe token is not empty
+		if(!empty($_POST['stripeToken']))
+		{
+			//get token, card and user info from the form
+			$token  = $_POST['stripeToken'];
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$card_num = $_POST['card_num'];
+			$card_cvc = $_POST['cvc'];
+			$card_exp_month = $_POST['exp_month'];
+			$card_exp_year = $_POST['exp_year'];
+			
+			//include Stripe PHP library
+			require_once APPPATH."third_party/stripe/init.php";
+			
+			//set api key
+			$stripe = array(
+         "secret_key"      => "sk_test_1r0I0HMdaVjbTX2t0QsZG4Ql006PiThqg6",
+		"publishable_key" => "pk_test_n5kGBnceS2DODJQlAUjLnIgQ00pebk2TNy"
+			);
+			
+			\Stripe\Stripe::setApiKey($stripe['secret_key']);
+			
+			//add customer to stripe
+			$customer = \Stripe\Customer::create(array(
+				'email' => $email,
+				'source'  => $token
+			));
+			
+			//item information
+			$itemName = "Stripe Donation";
+			$itemNumber = "PS123456";
+			$itemPrice = 50;
+			$currency = "usd";
+			$orderID = "SKA92712382139";
+			
+			//charge a credit or a debit card
+			$charge = \Stripe\Charge::create(array(
+				'customer' => $customer->id,
+				'amount'   => $itemPrice,
+				'currency' => $currency,
+				'description' => $itemNumber,
+				'metadata' => array(
+					'item_id' => $itemNumber
+				)
+			));
+			
+			//retrieve charge details
+			$chargeJson = $charge->jsonSerialize();
+
+			//check whether the charge is successful
+			if($chargeJson['amount_refunded'] == 0 && empty($chargeJson['failure_code']) && $chargeJson['paid'] == 1 && $chargeJson['captured'] == 1)
+			{
+				//order details 
+				$amount = $chargeJson['amount'];
+				$balance_transaction = $chargeJson['balance_transaction'];
+				$currency = $chargeJson['currency'];
+				$status = $chargeJson['status'];
+				$date = date("Y-m-d H:i:s");
+			
+				
+				//insert tansaction data into the database
+				$dataDB = array(
+					'name' => $name,
+					'email' => $email, 
+					'card_num' => $card_num, 
+					'card_cvc' => $card_cvc, 
+					'card_exp_month' => $card_exp_month, 
+					'card_exp_year' => $card_exp_year, 
+					'item_name' => $itemName, 
+					'item_number' => $itemNumber, 
+					'item_price' => $itemPrice, 
+					'item_price_currency' => $currency, 
+					'paid_amount' => $amount, 
+					'paid_amount_currency' => $currency, 
+					'txn_id' => $balance_transaction, 
+					'payment_status' => $status,
+					'created' => $date,
+					'modified' => $date
+				);
+
+				if ($this->db->insert('orders', $dataDB)) {
+					if($this->db->insert_id() && $status == 'succeeded'){
+						$data['insertID'] = $this->db->insert_id();
+						$this->load->view('payment_success', $data);
+						// redirect('Welcome/payment_success','refresh');
+					}else{
+						echo "Transaction has been failed";
+					}
+				}
+				else
+				{
+					echo "not inserted. Transaction has been failed";
+				}
+
+			}
+			else
+			{
+				echo "Invalid Token";
+				$statusMsg = "";
+			}
+		}
+	}
+
+	public function payment_success()
+	{
+		$this->load->view('payment_success');
+	}
+
+	public function payment_error()
+	{
+		$this->load->view('payment_error');
+	}
+
+	public function help()
+	{
+		$this->load->view('help');
 	}
 }
---
+++ application/views/product_form.php
@@ -0,0 +1,175 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Stripe Gateway Integration | Codeigniter</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css" />    
+
+    <!-- jQuery is used only for this example; it isn't required to use Stripe -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" />
+
+    <!-- Stripe JavaScript library -->
+    <script type="text/javascript" src="https://js.stripe.com/v2/"></script>    
+    
+    <script type="text/javascript">
+        //set your publishable key
+        Stripe.setPublishableKey('pk_test_n5kGBnceS2DODJQlAUjLnIgQ00pebk2TNy');
+        
+        //callback to handle the response from stripe
+        function stripeResponseHandler(status, response) {
+            if (response.error) {
+                //enable the submit button
+                $('#payBtn').removeAttr("disabled");
+                //display the errors on the form
+                // $('#payment-errors').attr('hidden', 'false');
+                $('#payment-errors').addClass('alert alert-danger');
+                $("#payment-errors").html(response.error.message);
+            } else {
+                var form$ = $("#paymentFrm");
+                //get token id
+                var token = response['id'];
+                //insert the token into the form
+                form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+                //submit form to the server
+                form$.get(0).submit();
+            }
+        }
+        $(document).ready(function() {
+            //on form submit
+            $("#paymentFrm").submit(function(event) {
+                //disable the submit button to prevent repeated clicks
+                $('#payBtn').attr("disabled", "disabled");
+                
+                //create single-use token to charge the user
+                Stripe.createToken({
+                    number: $('#card_num').val(),
+                    cvc: $('#card-cvc').val(),
+                    exp_month: $('#card-expiry-month').val(),
+                    exp_year: $('#card-expiry-year').val()
+                }, stripeResponseHandler);
+                
+                //submit from callback
+                return false;
+            });
+        });
+    </script>
+
+
+	
+</head>
+<body>
+
+
+<h1><center>Stripe payment gateway integration in codeigniter</center></h1>
+	
+
+<div class="container">
+	<div class="row">	
+
+        <div class="col-md-4">
+            
+            <div class="card">
+                <div class="card-header bg-success text-white">Product Information</div>
+                <div class="card-body bg-light">
+                    <?php if (validation_errors()): ?>
+                        <div class="alert alert-danger" role="alert">
+                            <strong>Oops!</strong>
+                            <?php echo validation_errors() ;?> 
+                        </div>  
+                    <?php endif ?>
+                    <div id="payment-errors"></div>  
+                     <form method="post" id="paymentFrm" enctype="multipart/form-data" action="<?php echo base_url(); ?>Welcome/check">
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control" placeholder="Name" value="<?php echo set_value('name'); ?>" required>
+                        </div>  
+
+                        <div class="form-group">
+                            <input type="email" name="email" class="form-control" placeholder="email@you.com" value="<?php echo set_value('email'); ?>" required />
+                        </div>
+
+                         <div class="form-group">
+                            <input type="number" name="card_num" id="card_num" class="form-control" placeholder="Card Number" autocomplete="off" value="<?php echo set_value('card_num'); ?>" required>
+                        </div>
+                       
+                        
+                        <div class="row">
+
+                            <div class="col-sm-8">
+                                 <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <input type="text" name="exp_month" maxlength="2" class="form-control" id="card-expiry-month" placeholder="MM" value="<?php echo set_value('exp_month'); ?>" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <input type="text" name="exp_year" class="form-control" maxlength="4" id="card-expiry-year" placeholder="YYYY" required="" value="<?php echo set_value('exp_year'); ?>">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="form-group">
+                                    <input type="text" name="cvc" id="card-cvc" maxlength="3" class="form-control" autocomplete="off" placeholder="CVC" value="<?php echo set_value('cvc'); ?>" required>
+                                </div>
+                            </div>
+                        </div>
+                        
+
+                       
+
+                        <div class="form-group text-right">
+                          <button class="btn btn-secondary" type="reset">Reset</button>
+                          <button type="submit" id="payBtn" class="btn btn-success">Submit Payment</button>
+                        </div>
+                    </form>     
+                </div>
+            </div>
+                 
+        </div>
+
+        
+
+        <div class="col-md-4">
+        	<div class="card">
+        		<h6 class="card-header bg-primary text-white">
+        			Some Help?
+        		</h6>
+        		<div class="card-body">
+        			<p>Get some real help by browsing these guide from offical source.</p>
+        			<ol>
+        				<li> <a href="https://stripe.com/docs" target="_blank">Stripe Docs</a> </li>
+        				<li> <a href="https://stripe.com/docs/checkout" target="_blank">Stripe Checkout</a></li>
+        				<li> <a href="https://stripe.com/docs/error-codes" target="_blank">Stripe Error Codes</a></li>
+
+        			</ol>
+        		</div>
+        	</div>
+        </div>
+
+    </div>
+</div> 
+
+   
+
+<!-- Footer -->
+<!-- <footer class="footer">
+  <div class="container">
+    Copyright &copy; <?php //echo date('Y'); ?>  
+        <span class="float-right">Coded with Love &hearts;  : <a href="https://facebook.com/anburocky3" target="_blank">Anbuselvan Rocky</a></span>
+  </div>
+</footer> -->
+
+</body>
+</html>
